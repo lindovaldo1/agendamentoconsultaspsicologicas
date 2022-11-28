875226cd9637674754f0a83380c92bb895bd7328
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-11-28 11:44:30 -0300

    :hammer: created class service Paciente

---
+++ src/main/java/com/daw2/lindovaldo/service/PacienteService.java
@@ -0,0 +1,11 @@
+package com.daw2.lindovaldo.service;
+
+import com.daw2.lindovaldo.repository.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class PacienteService {
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+}
