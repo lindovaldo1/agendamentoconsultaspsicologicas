3f61a85ddf34e74305cc5658c4413192506e48e7
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-11-28 11:58:28 -0300

    :pop: error in aplication.properties banq

--- src/main/java/com/daw2/lindovaldo/Main.java
+++ src/main/java/com/daw2/lindovaldo/Main.java
@@ -1,7 +1,9 @@
 package com.daw2.lindovaldo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
