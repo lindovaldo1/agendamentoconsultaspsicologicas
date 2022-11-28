7ca38d488a913adfcf84d4a703ee989ca3c5116e
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-11-28 11:26:38 -0300

    :hammer: created class model Consulta

--- src/main/java/com/daw2/lindovaldo/model/ConsultaModel.java
+++ src/main/java/com/daw2/lindovaldo/model/ConsultaModel.java
@@ -1,4 +1,30 @@
 package com.daw2.lindovaldo.model;
 
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "consulta")
+@DynamicUpdate
 public class ConsultaModel {
+
+    @Id
+    @SequenceGenerator(name = "generator3", sequenceName = "consulta_code_seq", allocationSize = 1)
+    @GeneratedValue(generator = "generator3", strategy = GenerationType.SEQUENCE)
+    private Long code;
+
+    @ManyToOne
+    @JoinColumn(name="codigo")
+    private PacienteModel paciente;
+
+    @ManyToOne
+    @JoinColumn(name="codigo")
+    private PsicologoModel psicologo;
+
+    private LocalDateTime consultedTime;
+
+    @Enumerated(EnumType.STRING)
+    private SituationModel situation = SituationModel.NONE;
 }
