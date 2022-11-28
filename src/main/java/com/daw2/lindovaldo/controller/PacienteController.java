c537a9f46dd5afbd396ba946cd99d0b2ad527f73
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-11-28 11:43:44 -0300

    :hammer: created class controller Paciente

---
+++ src/main/java/com/daw2/lindovaldo/controller/PacienteController.java
@@ -0,0 +1,18 @@
+package com.daw2.lindovaldo.controller;
+
+import com.daw2.lindovaldo.repository.PacienteRepository;
+import com.daw2.lindovaldo.service.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/paciente")
+public class PacienteController {
+
+    @Autowired
+    private PacienteService pacienteService;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+}
