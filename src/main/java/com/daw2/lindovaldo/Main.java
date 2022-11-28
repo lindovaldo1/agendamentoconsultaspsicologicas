7587c0614c9b4e61153735222835004d8fa7cc4a
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-11-28 09:14:41 -0300

    :tada: initil commit

---
+++ src/main/java/com/daw2/lindovaldo/Main.java
@@ -0,0 +1,9 @@
+package com.daw2.lindovaldo;
+
+import org.springframework.boot.SpringApplication;
+
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
