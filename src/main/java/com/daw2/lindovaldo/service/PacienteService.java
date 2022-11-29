0ffff94a0cd47063db64b7dc01ceeb1e60dc78dc
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-11-29 11:48:57 -0300

    :bug: error in service he was include annotation @service in class

--- src/main/java/com/daw2/lindovaldo/service/PacienteService.java
+++ src/main/java/com/daw2/lindovaldo/service/PacienteService.java
@@ -1,8 +1,11 @@
 package com.daw2.lindovaldo.service;
 
+import com.daw2.lindovaldo.model.PacienteModel;
 import com.daw2.lindovaldo.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PacienteService {
 
     @Autowired
