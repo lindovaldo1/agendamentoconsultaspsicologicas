681c974d59936fb01bf29f521706be9069ec03a5
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-11-29 23:32:27 -0300

    :bug: fixed the sql sintaxe

--- src/main/resources/db.migration/V01__cria_tabela_paciente.sql
+++ src/main/resources/db.migration/V01__cria_tabela_paciente.sql
@@ -1,8 +1,8 @@
 CREATE TABLE public.paciente
-{
+(
     codigo bigserial NOT NULL,
     fisrtName text,
     lastName text,
     cpf text,
     PRIMARY KEY (codigo)
-}
+);
--- src/main/resources/db.migration/V02__cria_tabela_psicologo.sql
+++ src/main/resources/db.migration/V02__cria_tabela_psicologo.sql
@@ -1,9 +1,9 @@
 CREATE TABLE public.psicologo
-{
+(
     codigo bigserial NOT NULL,
     fisrtName text,
     lastName text,
     cpf text,
     specialty text DEFAULT 'NONE',
     PRIMARY KEY (codigo)
-}
+);
