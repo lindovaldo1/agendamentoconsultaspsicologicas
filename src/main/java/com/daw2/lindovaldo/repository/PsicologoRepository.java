150882a1cdaee722dd4a920e3a5872424d9cf97b
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-12-05 20:01:49 -0300

    :hammer: created psicologo repository

---
+++ src/main/java/com/daw2/lindovaldo/repository/PsicologoRepository.java
@@ -0,0 +1,7 @@
+package com.daw2.lindovaldo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.daw2.lindovaldo.model.PsicologoModel;
+
+public interface PsicologoRepository extends JpaRepository<PsicologoModel, Long>{}
