338ba776b7750efb1cfec186315b27331c7e048d
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-12-06 15:29:16 -0300

    :pencil2: variables names

--- src/main/java/com/daw2/lindovaldo/model/filter/PsicologoFilter.java
+++ src/main/java/com/daw2/lindovaldo/model/filter/PsicologoFilter.java
@@ -4,36 +4,41 @@ import com.daw2.lindovaldo.model.Specialty;
 
 public class PsicologoFilter {
 
-    private Long codigo;
-    private String name;
-    private String cpf;
-    private Specialty specialty;
-    
-    
+	private Long codigo;
+	private String nome;
+	private String cpf;
+	private Specialty specialty;
+
 	public Long getCodigo() {
 		return codigo;
 	}
+
 	public void setCodigo(Long code) {
 		this.codigo = code;
 	}
-	public String getName() {
-		return name;
+
+	public String getNome() {
+		return nome;
 	}
-	public void setName(String firstName) {
-		this.name = firstName;
-	}	
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public Specialty getSpecialty() {
 		return specialty;
 	}
+
 	public void setSpecialty(Specialty specialty) {
 		this.specialty = specialty;
 	}
-	
-    
+
 }
