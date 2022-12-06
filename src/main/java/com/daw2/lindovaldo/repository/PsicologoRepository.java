e970eaf59ec4d636391ac63524d34fe5371ee494
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-12-05 23:45:53 -0300

    :recycle: organize the repository

--- src/main/java/com/daw2/lindovaldo/repository/PsicologoRepository.java
+++ src/main/java/com/daw2/lindovaldo/repository/PsicologoRepository.java
@@ -3,5 +3,6 @@ package com.daw2.lindovaldo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.daw2.lindovaldo.model.Psicologo;
+import com.daw2.lindovaldo.repository.helper.PsicologoQueries;
 
-public interface PsicologoRepository extends JpaRepository<Psicologo, Long>{}
+public interface PsicologoRepository extends JpaRepository<Psicologo, Long>, PsicologoQueries{}
