7338396da9addd71a74388890c290fafd15e10f0
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-12-06 15:38:11 -0300

    :recycle: extends PacienteQueries in repository

--- src/main/java/com/daw2/lindovaldo/repository/PacienteRepository.java
+++ src/main/java/com/daw2/lindovaldo/repository/PacienteRepository.java
@@ -1,6 +1,9 @@
 package com.daw2.lindovaldo.repository;
 
 import com.daw2.lindovaldo.model.Paciente;
+import com.daw2.lindovaldo.repository.helper.paciente.PacienteQueries;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PacienteRepository extends JpaRepository<Paciente, Long>{}
+public interface PacienteRepository extends JpaRepository<Paciente, Long>, PacienteQueries {
+}
