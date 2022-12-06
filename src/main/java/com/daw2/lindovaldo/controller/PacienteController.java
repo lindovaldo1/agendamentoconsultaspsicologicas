e38f5cd225b673d0695408243324745f7a4e4ef9
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-12-06 15:42:04 -0300

    :construction: create search pacientes

--- src/main/java/com/daw2/lindovaldo/controller/PacienteController.java
+++ src/main/java/com/daw2/lindovaldo/controller/PacienteController.java
@@ -1,10 +1,24 @@
 package com.daw2.lindovaldo.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.daw2.lindovaldo.model.Paciente;
+import com.daw2.lindovaldo.model.filter.PacienteFilter;
 import com.daw2.lindovaldo.repository.PacienteRepository;
 import com.daw2.lindovaldo.service.PacienteService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+
+import pagination.PageWrapper;
 
 @Controller
 @RequestMapping("/pacientes")
@@ -15,4 +29,20 @@ public class PacienteController {
 
     @Autowired
     private PacienteRepository pacienteRepository;
+
+    @GetMapping("/abrirpesquisar")
+    public String abrirPesquisa() {
+        return "paciente/pesquisar";
+    }
+
+    @GetMapping("/pesquisar")
+    public String pesquisar(PacienteFilter filtro, Model model,
+            @PageableDefault(size = 10) @SortDefault(sort = "codigo", direction = Sort.Direction.ASC) Pageable pageable,
+            HttpServletRequest request) {
+        Page<Paciente> pagina = pacienteRepository.pesquisar(filtro, pageable);
+        PageWrapper<Paciente> paginaWrapper = new PageWrapper<>(pagina, request);
+        model.addAttribute("pagina", paginaWrapper);
+        return "paciente/mostrartodas";
+    }
+
 }
