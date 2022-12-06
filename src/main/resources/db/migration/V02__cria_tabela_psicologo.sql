a695c44484afc2b92e05c1da2ce81b5b418a0976
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-12-06 14:23:03 -0300

    :pencil2: typos insert sentece

--- src/main/resources/db/migration/V02__cria_tabela_psicologo.sql
+++ src/main/resources/db/migration/V02__cria_tabela_psicologo.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS public.psicologo
 (
     codigo bigserial NOT NULL,
-    name text,
+    nome text,
     cpf text,
     specialty text DEFAULT 'NONE',
     PRIMARY KEY (codigo)
 );
 
-INSERT INTO public.psicolog(name, cpf, specialty) VALUES
+INSERT INTO public.psicologo(nome, cpf, specialty) VALUES
 ('Grosbilda', '34987654321', 'PSICOLOGIA_SOCIAL'),
 ('Estrobilobaldo', '34912345678', 'NEUROPSICOLOGIA'),
 ('Zirgonisvaldo', '34912348765', 'PSICOLOGIA_SAUDE');
