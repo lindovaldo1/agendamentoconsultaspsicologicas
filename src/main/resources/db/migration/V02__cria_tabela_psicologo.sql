d7aefb3e6e0a248ac06718fc0f048de6eb972ccd
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-12-05 23:40:55 -0300

    :card_file_box: insert psicologos

--- src/main/resources/db/migration/V02__cria_tabela_psicologo.sql
+++ src/main/resources/db/migration/V02__cria_tabela_psicologo.sql
@@ -1,9 +1,13 @@
 CREATE TABLE IF NOT EXISTS public.psicologo
 (
     codigo bigserial NOT NULL,
-    fisrtName text,
-    lastName text,
+    name text,
     cpf text,
     specialty text DEFAULT 'NONE',
     PRIMARY KEY (codigo)
 );
+
+INSERT INTO public.psicolog(name, cpf, specialty) VALUES
+('Grosbilda', '34987654321', 'PSICOLOGIA_SOCIAL'),
+('Estrobilobaldo', '34912345678', 'NEUROPSICOLOGIA'),
+('Zirgonisvaldo', '34912348765', 'PSICOLOGIA_SAUDE');
