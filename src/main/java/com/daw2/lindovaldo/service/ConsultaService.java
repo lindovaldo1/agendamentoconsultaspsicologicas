77d18befef09f303ef7a1a86f6ed89aac5243853
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-12-08 15:42:21 -0300

    :hammer: code alterar and salvar in data base

--- src/main/java/com/daw2/lindovaldo/service/ConsultaService.java
+++ src/main/java/com/daw2/lindovaldo/service/ConsultaService.java
@@ -1,5 +1,6 @@
 package com.daw2.lindovaldo.service;
 
+import com.daw2.lindovaldo.model.Consulta;
 import com.daw2.lindovaldo.repository.ConsultaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,5 +10,11 @@ public class ConsultaService {
 
     @Autowired
     private ConsultaRepository consultaRepository;
+
+    public void salvar(Consulta consulta) {
+    }
+
+    public void alterar(Consulta consulta) {
+    }
 
 }
