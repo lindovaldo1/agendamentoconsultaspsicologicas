566be7a2006f125cd3fe6994518474febd520d62
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-12-13 01:41:56 -0300

    :card_file_box: create tables to users and papers

---
+++ src/main/resources/db/migration/V05__insercao_dados_padrao_spring.sql
@@ -0,0 +1,14 @@
+INSERT INTO public.usuario (nome, email, senha, nome_usuario, ativo, data_nascimento) VALUES
+('Grosbilda', 'grosbilda@seilah.com', '{noop}12345', 'grosbilda', true, '2000-06-06'),
+('Estrobilobaldo', 'estrobilobaldo@seilah.com', '{noop}12345', 'estrobilobaldo', true, '2001-06-12'),
+('Zirgonisvaldo', 'zirgonisvaldo@seilah.com', '{noop}12345', 'zirgonisvaldo', true, '2002-06-11');
+
+INSERT INTO public.papel (codigo, nome) VALUES 
+(1, 'ROLE_ADMIN'),
+(2, 'ROLE_USUARIO');
+
+INSERT INTO public.usuario_papel (codigo_usuario, codigo_papel) VALUES
+(1, 1),
+(2, 2),
+(3, 1),
+(3, 2);
