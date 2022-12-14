813d37d244094d28da635a6026675427214afd34
lindovaldo1 <lindovaldo.leao@estudante.iftm.edu.br>
2022-12-14 09:20:56 -0300

    :hammer: login

--- src/main/java/com/daw2/lindovaldo/controller/IndexController.java
+++ src/main/java/com/daw2/lindovaldo/controller/IndexController.java
@@ -6,6 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class IndexController {
 
+	@GetMapping(value = {"/", "/index.html"} )
+	public String index() {
+		return "index";
+	}
+	
 	@GetMapping("/login")
 	public String login() {
 		return "login";
